ca227487f8369fbc34541778be3cd4099c554e07
alamin5g <alaminrakib4864@gmail.com>
2024-02-01 22:53:13 +0600

    initial

--- src/com/goonok/ATM.java
+++ src/com/goonok/ATM.java
@@ -1,7 +1,21 @@
 package com.goonok;
 
+import java.util.Scanner;
+
 public class ATM {
     private  double balance;
-    private int PIN;
-    
+    private int PIN = 1234;
+
+    public void checkPin(){
+        System.out.print("Enter your PIN");
+        Scanner input = new Scanner(System.in);
+        int checkPin = input.nextInt();
+        if (checkPin==PIN){
+            menu();
+        }
+    }
+
+    private void menu() {
+    }
+
 }
